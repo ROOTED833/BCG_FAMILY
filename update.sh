493d77e4f9f5a1909620dd7ca08c2863c5408013
ROOTED833 <38461668+ROOTED833@users.noreply.github.com>
2018-09-30 12:13:38 +0600

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,53 @@
+#
+#  ____   ____ ____   _____ _    __  __ ___ _  __   __
+# | __ ) / ___/ ___| |  ___/ \  |  \/  |_ _| | \ \ / /
+# |  _ \| |  | |  _  | |_ / _ \ | |\/| || || |  \ V /
+# | |_) | |__| |_| | |  _/ ___ \| |  | || || |___| |
+# |____/ \____\____| |_|/_/   \_\_|  |_|___|_____|_|
+#     ~ This tools for BCG FAMILY by WPD
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/BCG_FAMILY/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/BCG_FAMILY
+
+clear
+
+mkdir BCG
+
+clear
+
+cd BCG
+
+clear
+
+git clone https://github.com/ROOTED833/BCG_FAMILY.git
+
+clear
+
+cd BCG_FAMILY
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
